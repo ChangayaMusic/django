4f0f922597d36a4afe969bb10da17c063d6743f1
ChangayaMusic <121641916+ChangayaMusic@users.noreply.github.com>
2023-09-19 14:02:21 +0200

    first users models

--- reviews/network/models.py
+++ reviews/network/models.py
@@ -1,12 +1,12 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import User  # Import auth.User
 
 class Ticket(models.Model):
     # Your Ticket model definition goes here
     pass
-
 
 class Review(models.Model):
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
@@ -19,11 +19,56 @@ class Review(models.Model):
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('L\'adresse e-mail est obligatoire')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Le superuser doit avoir is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Le superuser doit avoir is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    # Specify related names to avoid clashes with auth.User
+    groups = models.ManyToManyField(
+        'auth.Group', related_name='custom_users', blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission', related_name='custom_users', blank=True
+    )
+
+    # Add other custom fields here if needed
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
 
 class UserFollows(models.Model):
-    # Your UserFollows model definition goes here
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='following')
+    followed_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='followers')
 
     class Meta:
-        # ensures we don't get multiple UserFollows instances
-        # for unique user-user_followed pairs
-        unique_together = ('user', 'followed_user', )
+        unique_together = ('user', 'followed_user',)
